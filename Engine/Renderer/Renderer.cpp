ce5939bb91dd68cbf1ce98fe56fac4e667fbcb62
chibichibichaba <48870208+chibichibichaba@users.noreply.github.com>
2025-10-11 19:40:26 +0200

    getting wet feet

---
+++ Engine/Renderer/Renderer.cpp
@@ -0,0 +1,8 @@
+#include "Renderer.h"
+#include <SFML/Graphics.hpp>
+
+
+void Renderer::openWindow(int height, int width, std::string windowname = "Hello world"){
+    
+
+};
---
+++ Engine/Renderer/Renderer.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <string>
+
+class Renderer{
+    
+    
+
+    public:
+       
+        void openWindow(int height, int width, std::string windowname = "Hello world");
+        void closeWindow();
+
+
+ 
+
+};
+
+
