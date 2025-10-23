2e35ce2538a243a1be518e4af4ad57a02dd8e033
Vuntorion <52784374+Vuntorion@users.noreply.github.com>
2025-10-23 22:56:38 +0200

    first component manager

--- Engine/Scene/ECS.h
+++ Engine/Scene/ECS.h
@@ -1,28 +1,28 @@
 #pragma once
 #include <string>
 #include <cinttypes>
-#include <unordered_map>
 
 struct Entity{
-    uint32_t EnityID;
-    std::string name;
+
+    
 };
 
 
 class EntityManager{
-    
-    public:
-
-    Entity create(const std::string& name) ;
-    void kill();
-
-
-
 
 };
 
 
+template<typename component>
 class ComponentManager{
+    
+    public:
+    void add(EntityID e, const component& c);
+    void remove(EntityID e); 
+    component& get(EntityID e);
 
+    private:
+        std::unordered_map<EntityID, component> components;
+        
 };
 
