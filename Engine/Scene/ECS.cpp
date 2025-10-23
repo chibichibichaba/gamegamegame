7f64554f78a5f5d959a1acd34ffbf4f2d461d529
chibichibichaba <48870208+chibichibichaba@users.noreply.github.com>
2025-10-23 22:52:01 +0200

    entity manager started

--- Engine/Scene/ECS.cpp
+++ Engine/Scene/ECS.cpp
@@ -1,2 +1,21 @@
 #include "ECS.h"
+#include <iostream> //for debugging
 
+
+Entity EntityManager::create(const std::string& name){
+
+    std::unordered_map<std::string, uint32_t> entity;
+    
+    if(entity.size() == 0){
+        uint32_t EntityID = 0;
+        entity.insert({name, EntityID});
+    }
+
+    else {
+        uint32_t EntityID = entity.size();
+        entity.insert({name, EntityID});
+    }
+};
+
+
+void EntityManager::kill(){};
--- Engine/Scene/ECS.h
+++ Engine/Scene/ECS.h
@@ -1,14 +1,23 @@
 #pragma once
 #include <string>
 #include <cinttypes>
+#include <unordered_map>
 
 struct Entity{
-
-    
+    uint32_t EnityID;
+    std::string name;
 };
 
 
 class EntityManager{
+    
+    public:
+
+    Entity create(const std::string& name) ;
+    void kill();
+
+
+
 
 };
 
